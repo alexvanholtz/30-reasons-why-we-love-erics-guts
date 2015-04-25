147e08ed4ff152a749ca5528a108848aa72497af
Alex Van Holtz <alexvanholtz@gmail.com>
2015-04-25 09:20:21 -0400

    put sticky scroll back in doc ready to get rid of icky popping

--- assets/js/core.js
+++ assets/js/core.js
@@ -10,24 +10,24 @@
 		  $('#results').removeClass('wow');
 	}
 
-	//Sticky selectors on scroll
-	$(window).scroll(function () {
-
-		if ( $(this).scrollTop() > $('#header').outerHeight() ) {
-			$('#results').addClass('fixed');
-			$('#reasons').css('margin-top', $('#results').outerHeight());
-		}	
-		else{
-			$('#results').removeClass('fixed');
-			$('#reasons').css('margin-top', 0);
-		}
-
-	});
-
 	//Init animations and create scroll to top click event
 	$(document).ready(function(){
 
 	 	new WOW().init();
+
+		//Sticky selectors on scroll
+		$(window).scroll(function () {
+
+			if ( $(this).scrollTop() > $('#header').outerHeight() ) {
+				$('#results').addClass('fixed');
+				$('#reasons').css('margin-top', $('#results').outerHeight());
+			}	
+			else{
+				$('#results').removeClass('fixed');
+				$('#reasons').css('margin-top', 0);
+			}
+
+		});
 
 		$("a[href='#top']").click(function() {
 			$("html, body").animate({ scrollTop: 0 }, "slow");
