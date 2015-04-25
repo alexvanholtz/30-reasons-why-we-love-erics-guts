12fd6cc3ba5f10fd39e2b90513f7d78787d94999
Alex Van Holtz <alexvanholtz@gmail.com>
2015-04-24 21:51:23 -0400

    sticked the tag selectors on scroll and some other snazzy stuff i can't remember

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,7 +1,39 @@
-$(document).ready(function(){
+//Stick all dat js in an IIFE
+(function(){
 
-  
- new WOW().init();
+	var position = $(window).scrollTop();
+	var headerHeight = $('header#header').outerHeight();
 
+	//Remove animatey classes if we've already scrolled down the page
+	if( position > headerHeight ){
+		  $('#results').removeClass('fadeIn');
+		  $('#results').removeClass('wow');
+	}
 
-});
+	//Sticky selectors on scroll
+	$(window).scroll(function () {
+
+		if ( $(this).scrollTop() > $('#header').outerHeight() ) {
+			$('#results').addClass('fixed');
+			$('#reasons').css('margin-top', $('#results').outerHeight());
+		}	
+		else{
+			$('#results').removeClass('fixed');
+			$('#reasons').css('margin-top', 0);å
+		}
+
+	});
+
+	//Init animations and create scroll to top click event
+	$(document).ready(function(){
+
+	 	new WOW().init();
+
+		$("a[href='#top']").click(function() {
+			$("html, body").animate({ scrollTop: 0 }, "slow");
+			return false;
+		});
+
+	});
+
+})();
